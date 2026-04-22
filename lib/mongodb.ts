44dda5278657145321d9792c6d8fc560abb418c3
shuhaibtpwandoor@gmail.com <Shu@&123ADMIN>
2026-04-22 15:48:25 +0530

    fix mongodb ts error

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,5 +1,12 @@
 import mongoose from 'mongoose';
 
+declare global {
+  var mongoose: {
+    conn: any;
+    promise: Promise<any> | null;
+  };
+}
+
 const MONGODB_URI = process.env.MONGODB_URI!;
 
 if (!MONGODB_URI) {
